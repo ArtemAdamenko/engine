3294ac6f81c08a6131cc9375f932f927b1773728
Artem <adamenko.artem@gmail.com>
2012-09-14 18:14:41 +0400

    Mini commit

--- app/modules/Page/pageModule.php
+++ app/modules/Page/pageModule.php
@@ -3,11 +3,11 @@ include_once('/config/define.inc.php');
 /*Класс модуля страниц
 @author Artem Adamenko <artem.adamenko@gmail.com>
 */
-class pageModule extends Router{
+class pageModule extends Controller{
     /*@param $routes Хранит конфигурацию маршрутов.*/
     private static $routes;
     /*@param $template шаблон по умолчанию.*/
-    private static $template = 'index';
+    private static $template;
     /*@param MODULE Константа имени модуля.*/
     const MODULE = 'page';
 
@@ -15,8 +15,10 @@ class pageModule extends Router{
      * Маршрутизация по контроллерам
      */
     public static function pageRoute(){
+        parent::logging(self::MODULE);
         self::$routes = include('routes.php');
         $uri = parent::getURI();
+
         foreach(self::$routes as $pattern => $route){
             if(preg_match("~$pattern~", $uri)){
                 $internalRoute = preg_replace("~$pattern~", $route, $uri);
@@ -25,10 +27,8 @@ class pageModule extends Router{
                     $site = array_shift($parameters);                             //(localhost/engine, должно быть engine,лишний элемент)
                 }
                 $controller = array_shift($parameters);
-                $id = array_shift($parameters);
-                if (!(validation::isInteger($id))){
-                    self::$template = $id;
-                }
+                parent::setTemplate(array_shift($parameters));
+
                 $path = ROOT.'app/modules/'.self::MODULE.'/controllers/'.$controller.'.php';
                 if (file_exists($path)){
                     include($path);
